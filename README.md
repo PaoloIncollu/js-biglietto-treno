18515b3a1048adba0e6c887a87abb72044033977
PaoloIncollu <paoloinco@gmail.com>
2024-07-11 17:52:26 +0200

    esercizio completato

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# template-html-css-js
+
+Step da seguire
+
+1. Chiedere all'utente il numero di chilometri che vuole percorrere
+
+2. Chiedere l'età del passeggero 
+
+3. Impostare const prezzo al km e calcolare il prezzo biglietto privo di sconto
+
+4. Calcolare il prezzo del biglietto, applicando lo sconto dove previsto
+
+5. Output del prezzo finale in forma umana
+
+class Test {
+   public static void main (String [] args) {
+ 
+    double num = 12.5249;
+    double dueDecimali = Math.round(num*100.0)/100.0;
+    System.out.println (dueDecimali);
+
+  }
+}
--- java/scripts.js
+++ java/scripts.js
@@ -1,2 +1,45 @@
 
-console.log('java collegato')
+// 1Step
+
+const km = prompt('Quanti chilometri vuoi percorrere');
+console.log ('Chilometri che si vuole percorrere:', km );
+
+//2 Step
+const age = prompt("Inserisci l'età del passeggero");
+console.log('Età del passeggero:', age);
+
+// Step 3
+
+const prezzoKm = (0.21);
+
+let prezzoBiglietto = (km * prezzoKm);
+
+
+if (age < 18) {
+
+    prezzoBiglietto= (prezzoBiglietto - (prezzoBiglietto * ( 20 / 100) ));
+    let prezzo = prezzoBiglietto;
+    let prezzoDueDecimali = Math.round(prezzo*100.0)/100.0;
+    document.getElementById ('prezzo-biglietto').innerHTML= prezzoDueDecimali;
+
+}
+
+else if (age >= 65) {
+
+    prezzoBiglietto= (prezzoBiglietto - (prezzoBiglietto * ( 40 / 100) ));
+    let prezzo = prezzoBiglietto;
+    let prezzoDueDecimali = Math.round(prezzo*100.0)/100.0;
+    document.getElementById ('prezzo-biglietto').innerHTML= prezzoDueDecimali;
+
+}
+
+else{
+
+    
+    let prezzo = prezzoBiglietto;
+    let prezzoDueDecimali = Math.round(prezzo*100.0)/100.0;
+    document.getElementById ('prezzo-biglietto').innerHTML= prezzoDueDecimali ;
+}
+
+   
+      
